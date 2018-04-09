39766756ab75575a9a41012c47af55bd0f6fe4b0
nekottyo <nekottyo@gmail.com>
2018-04-09 20:15:52 +0900

    add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@user = User.new
+@user.name = 'Nekottyo'
+@user.username = 'nekottyo'
+@user.location = 'Tokyo, Japan'
+@user.about = 'Hello, I am nekottyo. I am from database!'
+@user.save
+
+@user = User.new
+@user.name = 'Shohei Aoki'
+@user.username = 'moyahima'
+@user.location = 'Tottori, Japan'
+@user.about = 'Nice to meet you. I am from database!'
+@user.save
