1ab47bdaff1a2513846128dd77017e7a308798f6
nekottyo <nekottyo@gmail.com>
2018-04-09 19:49:28 +0900

    route!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,10 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = Hash.new
+    @user[:name] = 'Nekottyo'
+    @user[:username] = 'nekottyo'
+    @user[:location] = 'Tokyo, Japan'
+    @user[:about] = 'Nice to meet you'
   end
 end
