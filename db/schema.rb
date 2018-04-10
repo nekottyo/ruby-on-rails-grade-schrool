03c84d5c9ebfa3ffa321cdaead8db1d45c48b0a3
nekottyo <nekottyo@gmail.com>
2018-04-10 11:17:08 +0900

    twitter っぽいやつ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409110638) do
+ActiveRecord::Schema.define(version: 20180409112734) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "title"
+    t.string "context"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
